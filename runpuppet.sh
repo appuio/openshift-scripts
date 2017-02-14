38639cd483b8af5d5d6267d76f3815274a684e4f
Reusser Lukas, IT222 <lukas.reusser@post.ch>
2017-02-14 14:09:45 +0100

    Load proxy config if available.

--- runpuppet.sh
+++ runpuppet.sh
@@ -1,1 +1,3 @@
+[ -e /etc/profile.d/proxy.sh ] && source /etc/profile.d/proxy.sh
+
 puppet apply -t --hiera_config /root/openshift-scripts/hiera.yaml --modulepath=/root/openshift-scripts/modules /root/openshift-scripts/manifests/site.pp
